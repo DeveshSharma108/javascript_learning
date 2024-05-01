a44cfd47f0b38d32af0b33ef02352f984a26f324
Devesh Sharma <137079248+DeveshSharma108@users.noreply.github.com>
2024-05-01 11:31:52 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // code will be treated as newer JS version code 
+
+/* alert(3+3)  wil generate error if we run on node if we run on browser runs fine, compiler of js is in-built in browsers 
+                node is dedicated for executing js in local machine */
+
+let name_="devesh"
+let age=21
+let isLoggedIn=true
+console.log(name_)  // why was deprecated coming when I was using name instead of name_ as variable 
+
+// number, bigint, string, boolean, null, undefined, symbol, object
+
+console.log(typeof name_)
+console.log(typeof null)
+console.log(typeof undefined)
