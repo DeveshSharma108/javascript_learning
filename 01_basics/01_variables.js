9ca07ee2068fc7635f0ddc0ec3077bc3c6c4bb60
Devesh Sharma <137079248+DeveshSharma108@users.noreply.github.com>
2024-05-01 11:31:30 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+// variables in javascript can be created by 3 ways const, let, var all of them are slightly different
+const id=10101
+let name_="Devesh"                         // what is difference between var and let ?
+var branch="Artificial Intelligence And Machine Learning"
+city="Gwalior"                             // which type of variable is city because we have not specified it
+let job
+console.log(id)
+console.log(name_,branch)
+console.table([id,name_,branch,city,job])
+
+/* prefer using let insted of var
+because of issue in block scope? and functional scope? while using var */
