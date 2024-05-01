0bc271d5c9837be7360d9f96dee784d5dbfc7ab6
Devesh Sharma <137079248+DeveshSharma108@users.noreply.github.com>
2024-05-01 13:12:24 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -18,3 +18,35 @@ console.log(Number(a))  // null get converted to 0 as a number; undefined --> Na
 
 let loggedIn=1
 console.log(typeof(Boolean(loggedIn)),Boolean(loggedIn))
+
+/***************************** Operations *********************************************************/
+
+// +,-,/,*,**,%,++,--
+
+console.log("hi "+"bye")
+console.log("hi "*3)  // would not output hi hi hi
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)        //you can read the ecma script documentation for rules of conversion by the way do not write 
+console.log(2 + 2 + "1")        //such confusing code instead use () to specify what has to be done first
+console.log(2 + "3" + 1)
+console.log(2 + 3 + "3" + 1)
+console.log(+true)
+console.log(+"")
+
+let num=10
+//num++ or ++num both giving 11 why? 
+console.log(num)
+
+let x = 3;
+x++;
+
+console.log(`x:${x}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
