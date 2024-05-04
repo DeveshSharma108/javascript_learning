cb4c5d8361938847f5d9f3046ff31a2eb361282d
Devesh Sharma <137079248+DeveshSharma108@users.noreply.github.com>
2024-05-04 10:06:24 +0000

    numbers and maths

---
+++ 01_basics/07_numbersAndMaths.js
@@ -0,0 +1,57 @@
+// let num1 = 100
+// let num2 = new Number(100)
+// console.log(num1)
+// console.log(num2)
+// console.log(typeof(num1))
+// console.log(typeof(num2))
+// console.log(num1==num2)
+// console.log(num1===num2)
+
+let num = Math.floor((Math.random()) * 100) + 1
+console.log(num)
+num = Math.pow(num,2)
+console.log(num)
+num = Math.round(Math.sin(num))
+console.log(num)
+num = Math.abs(num)
+num = num.toString()
+console.log("Magical nuumber: ",num)
+console.log(num,typeof(num))
+
+
+
+// const score = 400
+// // console.log(score);
+
+// const balance = new Number(100)
+// // console.log(balance);
+
+// // console.log(balance.toString().length);
+// // console.log(balance.toFixed(1));
+
+// const otherNumber = 123.8966
+
+// // console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 1000000
+// // console.log(hundreds.toLocaleString('en-IN'));
+
+// // +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+// // for generating a random number in a given range
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
