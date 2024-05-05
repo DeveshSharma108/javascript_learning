761b4c828dea4563b5b0ae05b745fcd4064734db
Devesh Sharma <137079248+DeveshSharma108@users.noreply.github.com>
2024-05-05 09:41:08 +0000

    done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -57,4 +57,58 @@ let sweet_boondi_recipe = {
 }
 
 
+console.log(sweet_boondi_recipe)
+console.log()
+
+sweet_boondi_recipe.getSpecialIngredients()
+console.log()
+
+console.log(sweet_boondi_recipe.Almonds)
+console.log()
+
 console.log(sweet_boondi_recipe.cooking_instructions)
+console.log()
+
+console.log(sweet_boondi_recipe.cooking_instructions[1])
+
+
+
+
+/*// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());*/
